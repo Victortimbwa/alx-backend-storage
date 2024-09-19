806b057a593eccb1dc5858275e31a2757855edc9
Victortimbwa <victortimbwa@gmail.com>
2024-09-19 09:21:23 +0300

    Email validation sent

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,7 @@
 -- Script that creates a trigger
 -- The trigger resets valid_email on email change
 DELIMETER $$
-CREAT TRIGGER validate_email BEFORE UPDATE ON users
+CREAT TRIGGER validate BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
 	IF NEW.EMAIL != OLD.email THEN
