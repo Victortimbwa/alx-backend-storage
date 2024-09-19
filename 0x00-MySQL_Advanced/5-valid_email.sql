7185d9ccb6426e283cbaca9fc189223d63636160
Victortimbwa <victortimbwa@gmail.com>
2024-09-19 09:09:59 +0300

    Email validation to sent

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- A script that creates a Trigger
+-- that resets the valid_email attribute
+DELIMETER $$ ;
+CREATE TRIGGER email_trigger BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF; 
+END;$$
+delimiter ;
